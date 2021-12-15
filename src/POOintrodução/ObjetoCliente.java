eb6af6674a2175d71c5e5b63559a88afdd1d0e01
unknown <contato.marcoscandido@gmail.com>
2021-12-15 13:26:03 -0300

    POO

---
+++ src/POOintrodução/ObjetoCliente.java
@@ -0,0 +1,25 @@
+package POOintrodução;
+
+public class ObjetoCliente {
+	
+	public static void main (String[]args){
+		 
+		Cliente Cliente1 = new Cliente(  "Marcos","Vinicius","Vieira");
+		Cliente1.idade = 20;
+		
+		Cliente1.sexo = "Masculino";
+	
+		
+	    
+		Cliente1.getCPFcadastrado();
+		//CPF ficticio
+		Cliente1.setCPFcadastrado(12345678910l);
+		Cliente1.getValidação();
+		Cliente1.setValidação(12345678912l);
+		Cliente1.confirmação();
+		Cliente1.status();
+	}
+
+}
+
+
